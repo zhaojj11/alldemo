84e1178f372c8171b18837d0c78aa624c984909d
zhaojj11 <zhaojunjie@pyjava.com>
2021-12-14 10:25:20 +0800

    :construction: democat功能

---
+++ democat/src/main/java/com/pyjava/demo/democat/web/BaseServlet.java
@@ -0,0 +1,38 @@
+package com.pyjava.demo.democat.web;
+
+import com.pyjava.demo.democat.constants.HttpMethod;
+
+import java.io.IOException;
+
+/**
+ * @author zhaojj9
+ */
+public abstract class BaseServlet {
+    /**
+     * get方法
+     *
+     * @param request  请求
+     * @param response 响应
+     * @throws IOException io异常
+     */
+    public abstract void doGet(Request request, Response response) throws IOException;
+
+    /**
+     * post方法
+     *
+     * @param request  请求
+     * @param response 响应
+     * @throws IOException io异常
+     */
+    public abstract void doPost(Request request, Response response) throws IOException;
+
+    public void service(Request request, Response response) throws IOException, NoSuchMethodException {
+        if (HttpMethod.GET.equals(request.getMethod())) {
+            doGet(request, response);
+        } else if (HttpMethod.POST.equals(request.getMethod())) {
+            doPost(request, response);
+        } else {
+            throw new NoSuchMethodException("not support");
+        }
+    }
+}
